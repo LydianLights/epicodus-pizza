ff7925ead9f687a8fb9ea839f98b4b901d9f7b98
LydianLights <rane.fields@gmail.com>
2017-09-29 10:18:48 -0700

    Add topping selector

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
+// Pizza properties
 // id = programatic identifier
 // text = user-readable description
 function PizzaSize(id, text) {
+  this.id = id;
+  this.text = text;
+}
+
+function PizzaTopping(id, text) {
   this.id = id;
   this.text = text;
 }
@@ -10,7 +16,25 @@ var pizzaSizes = [
   new PizzaSize("sm", "Small"),
   new PizzaSize("md", "Medium"),
   new PizzaSize("lg", "Large"),
-  new PizzaSize("xl","Extra Large")];
+  new PizzaSize("xl","Extra Large")
+];
+
+  var pizzaToppings = [
+    new PizzaTopping("pepperoni", "Pepperoni"),
+    new PizzaTopping("chicken", "Chicken"),
+    new PizzaTopping("beef", "Beef"),
+    new PizzaTopping("sausage", "Sausage"),
+    new PizzaTopping("bacon", "Bacon"),
+    new PizzaTopping("anchovies", "Anchovies"),
+    new PizzaTopping("ham", "Ham"),
+
+    new PizzaTopping("pineapple", "Pineapple"),
+    new PizzaTopping("tomatoes", "Tomatoes"),
+    new PizzaTopping("mushrooms", "Mushrooms"),
+    new PizzaTopping("onions", "Onions"),
+    new PizzaTopping("olives", "Olives"),
+    new PizzaTopping("peppers", "Peppers")
+  ];
 
 // UI generation
 function generatePizzaSizes() {
@@ -25,7 +49,20 @@ function generatePizzaSizes() {
   return html;
 }
 
+function generatePizzaToppings() {
+  html = '<h3>Choose your toppings</h3>';
+  pizzaToppings.forEach(function(topping) {
+    html +=
+    `<p>
+    <input type="checkbox" class="filled-in" name="pizza-topping" id="pizza-topping-` + topping.id + `" value="` + topping.id + `">
+    <label for="pizza-topping-` + topping.id + `">` + topping.text + `</label>
+    </p>`;
+  });
+  return html;
+}
+
 
 $(document).ready(function() {
   $("#pizza-size-select").append(generatePizzaSizes());
+  $("#pizza-toppings-select").append(generatePizzaToppings());
 });
